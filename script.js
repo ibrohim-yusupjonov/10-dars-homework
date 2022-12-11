c9451f8b3dd96f558fdb5bf1e8bb2fa0ac10d7ff
ibrohim-yusupjonov <110836470+ibrohim-yusupjonov@users.noreply.github.com>
2022-12-11 22:29:02 +0500

    toliq emas

---
+++ script.js
@@ -0,0 +1,31 @@
+// function num() {
+//       for (son = 1; son <= 50; son++) {
+//           if (son % 2 == 0) {
+//               console.log(son + ' = bu juft son')
+//           } else {
+//               console.log(son + ' = bu toq son')
+//           }
+//       }
+//   }
+// num()
+
+
+// let ism = ["Abdurahmon", "Abdulloh", "Ibrohim", "Dinoza", "Ali"]
+// let startname = prompt("Ro'yxatdagi ismni topish uchun ism kiriting;")
+// for (let i = 0; i < ism.length; i++) {
+//     if (ism[i].startsWith(startname)){
+//         console.log(ism[i])
+//     }
+// }
+
+
+
+
+// let yozilganText = document.getElementById("text");
+// let yozilganSoz = document.getElementById("son");
+// function func(){
+//     let text = yozilganText.value
+//     text = text.trim()
+//     let matn = text.split("");
+//     yozilganSoz.innerHTML = matn.length;
+// }
